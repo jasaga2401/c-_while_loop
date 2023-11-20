64291a5a6e71f8b6f085fc5d39f93e54e28850d5
jasaga2401 <jasaga@protonmail.com>
2023-11-20 13:25:12 +0000

    Add project files.

---
+++ c#_while_loop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Initialize a counter variable
+        int counter = 1;
+
+        // Use a while loop to print numbers from 1 to 5
+        while (counter <= 5)
+        {
+            Console.WriteLine($"Current number: {counter}");
+
+            // Increment the counter
+            counter++;
+        }
+    }
+}
